448b2fef421f202d90448e96f38501170081f18f
Atul-Rathour <your-email@example.com>
2024-11-16 19:11:16 +0530

    navbar and menupanel complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Footer from "./Components/Footer";
 import "./App.css";
 import Home from "./Pages/Home";
 import Navbar from "./Components/Navbar";
 import Intro from "./Components/Intro";
 import Cursor from "./Components/Cursor/Cursor";
+import Menu from "./Components/Menu";
+import MenuPanel from "./Components/MenuPanel";
 
 const App = () => {
   const [isLoading, setIsloading] = useState(true);
   const [introComplete, setIntroComplete] = useState(false);
+  const MenuRef = useRef()
+
+  const handleOpen = () => {
+    MenuRef.current.openMenu();
+    MenuRef.current.openMenu();
+  };
+
 
   useEffect(() => {
     const loadAssets = async () => {
@@ -34,7 +43,9 @@ const App = () => {
       {introComplete && (
         <div  >
         <Cursor />
-          <Navbar />
+          <Navbar handleOpen={handleOpen} />
+          {/* <MenuPanel ref={MenuRef} /> */}
+          {/* <Menu ref={MenuRef} /> */}
           <Home />
           <Footer />
         </div>
